e7c627f0a369ca49f9dd9be3244f29b30b11edc4
NaimurRahmannn <niamurreiman@gmail.com>
2026-06-20 21:29:49 +0600

    feat: add home views in property_app

--- property_app/views.py
+++ property_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Property
+
+
+def home(request):
+    featured = (
+        Property.objects.filter(is_active=True)
+        .select_related("location")
+        .prefetch_related("images")
+        .order_by("-created_at")[:6]
+    )
+    return render(request, "property_app/home.html", {"featured": featured})
